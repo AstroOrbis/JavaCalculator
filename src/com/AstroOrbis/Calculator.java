f504cda927dacce7d20acb103afea969d4b13903
AstroOrbis <astroorbis@gmail.com>
2021-02-21 13:36:10 -0500

    Removed CalcTypeReadable cuz i found an easier way

--- src/com/AstroOrbis/Calculator.java
+++ src/com/AstroOrbis/Calculator.java
@@ -4,8 +4,6 @@ import java.util.*;
 import java.io.*;
 
 public class Calculator {
-    private static String CalcTypeReadable;
-
     public static void main(String[] args)
             throws IOException
     {
@@ -22,22 +20,18 @@ public class Calculator {
         //Making CalcType readable, and printing the readable version to console
         switch (CalcType) {
             case "+": {
-                String CalcTypeReadable = "Addition";
                 System.out.println("You chose: Addition");
                 break;
             }
             case "-": {
-                String CalcTypeReadable = "Substraction";
                 System.out.println("You chose: Substraction");
                 break;
             }
             case "/": {
-                String CalcTypeReadable = "Division";
                 System.out.println("You chose: Division");
                 break;
             }
             case "*": {
-                String CalcTypeReadable = "Multiplication";
                 System.out.println("You chose: Multiplication");
                 break;
             }
